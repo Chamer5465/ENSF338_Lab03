cd12a0f0f1a43f482336dd990ec9da7b3146f6b2
Chamer5465 <chasemack@shaw.ca>
2025-02-10 12:37:41 -0700

    worked on ex2

--- Exercise 2/ex2.py
+++ Exercise 2/ex2.py
@@ -1,5 +1,7 @@
 import random
 import timeit
+from statistics import median
+from matplotlib import pyplot as plt
 
 def bubble_sort(array):
     n = len(array)
@@ -9,9 +11,9 @@ def bubble_sort(array):
                 temp = array[j]
                 array[j] = array[j + 1]
                 array[j + 1] = temp
-    return array
 
-def quick_sort(array, low, high):
+def quick_sort(arr, low, high):
+    array = arr.copy()
     if low < high:
         pivot_index = partition(array, low, high)
         quick_sort(array, low, pivot_index)
@@ -36,16 +38,52 @@ def partition(array, low, high):
         return right
 
 def main():
-    for i in range (100, 2001, 100):
+    avg_bubble_best_times = []
+    avg_bubble_worst_times = []
+    avg_bubble_avg_times = []
+
+    avg_quick_best_times = []
+    avg_quick_worst_times = []
+    avg_quick_avg_times = []
+    
+    for i in range (10, 101, 5):
         data = [j for j in range(i)]
-        shuffled_data = random.shuffle(data)
-        reversed_data = data.reverse()
+        shuffled_data = data.copy()
+        reversed_data = list(reversed(data))
+        random.shuffle(shuffled_data)
+        shuffled_median = int(median(shuffled_data))
 
-        avg_bubble_time_avg_case = timeit.timeit(lambda:bubble_sort(data), number = 100) / 100
-        avg_quick_time_avg_case = timeit.timeit(lambda:quick_sort(data, 0, len(data) - 1), number = 100) / 100
+        avg_bubble_time_best_case = timeit.timeit(lambda:bubble_sort(data.copy()), number = 100) / 100
+        avg_quick_time_best_case = timeit.timeit(lambda:quick_sort(shuffled_data.copy(), shuffled_median , shuffled_median + 1), number = 100) / 100
 
-        print(f'The average bubble sort time for a sorted array of {i} elemets is {avg_bubble_time_avg_case}.')
-        print(f'The average quick sort time for an array of {i} elemets is {avg_quick_time_avg_case}.')
+        avg_bubble_time_worst_case = timeit.timeit(lambda:bubble_sort(reversed_data.copy()), number = 100) / 100
+        avg_quick_time_worst_case = timeit.timeit(lambda:quick_sort(shuffled_data.copy(), 0, 1), number = 100) / 100
+
+        avg_bubble_time_avg_case = timeit.timeit(lambda:bubble_sort(shuffled_data.copy()), number = 100) / 100
+        avg_quick_time_avg_case = timeit.timeit(lambda:quick_sort(data.copy(), 0, len(data) - 1), number = 100) / 100
+
+        avg_bubble_best_times.append(avg_bubble_time_best_case)
+        avg_bubble_worst_times.append(avg_bubble_time_worst_case)
+        avg_bubble_avg_times.append(avg_bubble_time_best_case)
+
+        avg_quick_best_times.append(avg_quick_time_best_case)
+        avg_quick_worst_times.append(avg_quick_time_worst_case)
+        avg_quick_avg_times.append(avg_quick_time_best_case)
+
+        print(f'The bubble sort time for a sorted array of {i} elemets is {avg_bubble_time_best_case}. ')
+        print(f'The bubble sort time for a reversed array of {i} elemets is {avg_bubble_time_worst_case}. ')
+        print(f'The bubble sort time for a shuffled array of {i} elemets is {avg_bubble_time_avg_case}.\n')
+
+        print(f'The quick sort time for an array of {i} elemets is using a random pivot is {avg_quick_time_avg_case}.')
+        print(f'The quick sort time for an array of {i} elemets is using a the lowest element as the pivot is {avg_quick_time_worst_case}.')
+        print(f'The quick sort time for an array of {i} elemets is using a random pivot is {avg_quick_time_best_case}.')
+    
+    x = [i for i in range (10, 101, 5)]
+
+    plt.figure()
+    plt.scatter(x= x, y= avg_bubble_avg_times)
+    plt.show()
+
 
 if __name__ == '__main__':
     main()
